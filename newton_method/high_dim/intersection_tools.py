1c19ceec3e40165f3e4d10709811d9c1a1d1a170
Сиднев Дмитрий <sidnev@rg.ru>
2022-10-10 11:06:17 +0300

    fix intersection algorithms

--- newton_method/high_dim/intersection_tools.py
+++ newton_method/high_dim/intersection_tools.py
@@ -12,26 +12,12 @@ def intersection(a: Interval, b: Interval):
     if isinstance(b, np.ndarray):
         b = b[0]
 
-    # проверка на отсутствие пересечения интервалов
-    if (Decimal(a[1]) < Decimal(b[0]) and Decimal(a[1]) < Decimal(b[1])) or\
-            (Decimal(b[1]) < Decimal(a[0]) and Decimal(b[1]) < Decimal(a[1])):
+
+    A_boundary = max(a[0], b[0])
+    B_boundary = min(a[1], b[1])
+
+    if A_boundary <= B_boundary:
+        return Interval([A_boundary, B_boundary])
+    else:
         return None
 
-    # проверка на нормальное пересечение
-    if Decimal(a[0]) < Decimal(b[0]) and Decimal(a[1]) < Decimal(b[1]):
-        return Interval([b[0], a[1]])
-
-    # проверка на нормальное пересечение №2
-    if Decimal(b[0]) < Decimal(a[0]) and Decimal(b[1]) < Decimal(a[1]):
-        return Interval([a[0], b[1]])
-
-    # проверка на вложенность
-    if a.isIn(b):
-        return a
-
-    # проверка на вложенность №2
-    if b.isIn(a):
-        return b
-
-    if a == b:
-        return a
--- newton_method/one_dim/intersection_tools.py
+++ newton_method/one_dim/intersection_tools.py
@@ -3,30 +3,14 @@ from practice_interval.interval_lib import Interval
 
 def intersection(a: Interval, b: Interval):
 
-    # проверка на отсутствие пересечения интервалов
-    if (a[1] < b[0] and a[1] < b[1]) or (b[1] < a[0] and b[1] < a[1]):
+    A_boundary = max(a[0], b[0])
+    B_boundary = min(a[1], b[1])
+
+    if A_boundary <= B_boundary:
+        return Interval([A_boundary, B_boundary])
+    else:
         return None
 
-    # проверка на нормальное пересечение
-    if a[0] < b[0] and a[1] < b[1]:
-        return Interval([b[0], a[1]])
-
-    # проверка на нормальное пересечение №2
-    if b[0] < a[0] and b[1] < a[1]:
-        return Interval([a[0], b[1]])
-
-    # проверка на вложенность
-    if a.isIn(b):
-        return a
-
-    # проверка на вложенность №2
-    if b.isIn(a):
-        return b
-
-    if a == b:
-        print("case 7")
-
-        return a
 
 
 
