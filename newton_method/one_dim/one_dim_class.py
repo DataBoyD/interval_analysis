adc7074b1cf616cdf47d9ebfdae67a4bf47f2691
Сиднев Дмитрий <sidnev@rg.ru>
2022-11-16 12:11:24 +0300

    fix: move repo in instance vars

--- newton_method/one_dim/one_dim_class.py
+++ newton_method/one_dim/one_dim_class.py
@@ -54,6 +54,8 @@ class NewtonIntervalIterationProcess:
 
         # сам итерационный процесс
         while result != None and result.width() > self.eps:
+
+            print(result.width())
             # print(f"RESULT: {result}\n"
             #       f"WIDTH: {result.width()}\n"
             #       f"EPS: {self.eps}")
@@ -96,70 +98,181 @@ class NewtonIntervalIterationProcess:
                     self.domain.common_base.append(result)
                     break
 
+    # def start_with_slope(self):
+    #
+    #     result = self.primary_interval
+    #
+    #     # сам итерационный процесс
+    #     while result != None and result.width() > self.eps:
+    #         # print(f"RESULT: {result}\n"
+    #         #       f"WIDTH: {result.width()}\n"
+    #         #       f"EPS: {self.eps}")
+    #
+    #         # вычисляем середину интервала $$\Tilde{x}$$
+    #         mid = (result[0] + result[1]) / 2
+    #         print("MID: ", mid)
+    #
+    #         triplet = Triplet(interval=result, value_at_c=Interval.valueToInterval(mid), slope=1)
+    #         print("TRIPLE: ", triplet )
+    #
+    #         # вычисляем обратный интервал к первой производной
+    #         division = 1 / self.f(triplet).slope
+    #
+    #         # Если содержится нуль, то интервал распадается на два луча
+    #         if isinstance(division, list):
+    #
+    #             # обрабатываем каждый луч отдельно (запускаем для них отдельные итерационные процессы)
+    #             for i in division:
+    #                 newt_iter = mid - self.f(mid) * i
+    #                 print("NEWT OPER: ", newt_iter)
+    #
+    #                 # newt_iter = Interval.valueToInterval(mid) - self.f(Interval.valueToInterval(mid)) * i
+    #
+    #                 t = NewtonIntervalIterationProcess(self.f,
+    #                                                    self.func_ext,
+    #                                                    self.der,
+    #                                                    intersection(result, newt_iter),
+    #                                                    self.domain,
+    #                                                    eps=self.eps)
+    #                 t.start_with_slope()
+    #             break
+    #         else:
+    #
+    #             # в обычной ситуации действуем по привычному алгоритму
+    #             triplet = Triplet(interval=result, value_at_c=Interval.valueToInterval(mid), slope=1)
+    #             newt_iter = mid - self.f(mid) / self.f(triplet).slope
+    #             print("TRIPLE: ", triplet)
+    #             print("NEWT OPER: ", newt_iter)
+    #             # newt_iter = Interval.valueToInterval(mid) - self.f(Interval.valueToInterval(mid)) / self.f(triplet).slope
+    #
+    #             # чтобы не допустить попадание None в массив результатов, добавляем данное условие
+    #             if intersection(result, newt_iter) is None:
+    #                 if result.width() <= self.eps:
+    #                     self.domain.common_base.append(result)
+    #                 break
+    #
+    #             result = intersection(result, newt_iter)
+    #             print("RESULT: ", result)
+    #             if result.width() <= self.eps:
+    #                 self.domain.common_base.append(result)
+    #                 break
+    # new version --------------------------------------
     def start_with_slope(self):
 
         result = self.primary_interval
 
         # сам итерационный процесс
         while result != None and result.width() > self.eps:
+
+            print(result.width())
+
             # print(f"RESULT: {result}\n"
             #       f"WIDTH: {result.width()}\n"
             #       f"EPS: {self.eps}")
 
             # вычисляем середину интервала $$\Tilde{x}$$
             mid = (result[0] + result[1]) / 2
-            print("MID: ", mid)
+            # print("MID: ", mid)
 
             triplet = Triplet(interval=result, value_at_c=Interval.valueToInterval(mid), slope=1)
-            print("TRIPLE: ", triplet )
+            print("TRIPLE: ", triplet)
 
-            # вычисляем обратный интервал к первой производной
-            division = 1 / self.f(triplet).slope
+            if isinstance(self.f(triplet), list):
+                for tr in self.f(triplet):
+                    # вычисляем обратный интервал к первой производной
+                    division = 1 / tr.slope
 
-            # Если содержится нуль, то интервал распадается на два луча
-            if isinstance(division, list):
+                    # Если содержится нуль, то интервал распадается на два луча
+                    if isinstance(division, list):
 
-                # обрабатываем каждый луч отдельно (запускаем для них отдельные итерационные процессы)
-                for i in division:
-                    newt_iter = mid - self.f(mid) * i
-                    print("NEWT OPER: ", newt_iter)
+                        # обрабатываем каждый луч отдельно (запускаем для них отдельные итерационные процессы)
+                        for i in division:
+                            newt_iter = mid - self.f(mid) * i
+                            # print("NEWT OPER: ", newt_iter)
 
-                    # newt_iter = Interval.valueToInterval(mid) - self.f(Interval.valueToInterval(mid)) * i
+                            # newt_iter = Interval.valueToInterval(mid) - self.f(Interval.valueToInterval(mid)) * i
+                            print("Вложенность")
 
-                    t = NewtonIntervalIterationProcess(self.f,
-                                                       self.func_ext,
-                                                       self.der,
-                                                       intersection(result, newt_iter),
-                                                       self.domain,
-                                                       eps=self.eps)
-                    t.start_with_slope()
-                break
+                            t = NewtonIntervalIterationProcess(self.f,
+                                                               self.func_ext,
+                                                               self.der,
+                                                               intersection(result, newt_iter),
+                                                               self.domain,
+                                                               eps=self.eps)
+                            t.start_with_slope()
+                        print(
+                            "Stop!!!!!!!!!!!!!!!!!!"
+                        )
+                        break
+                    else:
+
+                        # в обычной ситуации действуем по привычному алгоритму
+                        triplet = Triplet(interval=result, value_at_c=Interval.valueToInterval(mid), slope=1)
+                        newt_iter = mid - self.f(mid) / self.f(triplet)[0].slope
+                        # print("TRIPLE: ", triplet)
+                        # print("NEWT OPER: ", newt_iter)
+                        # newt_iter = Interval.valueToInterval(mid) - self.f(Interval.valueToInterval(mid)) / self.f(triplet).slope
+
+                        # чтобы не допустить попадание None в массив результатов, добавляем данное условие
+                        if intersection(result, newt_iter) is None:
+                            if result.width() <= self.eps:
+                                self.domain.common_base.append(result)
+                            break
+
+                        result = intersection(result, newt_iter)
+                        if result.width() <= self.eps:
+                            print("RESULT: ", result)
+                            self.domain.common_base.append(result)
+                            break
             else:
 
-                # в обычной ситуации действуем по привычному алгоритму
-                triplet = Triplet(interval=result, value_at_c=Interval.valueToInterval(mid), slope=1)
-                newt_iter = mid - self.f(mid) / self.f(triplet).slope
-                print("TRIPLE: ", triplet)
-                print("NEWT OPER: ", newt_iter)
-                # newt_iter = Interval.valueToInterval(mid) - self.f(Interval.valueToInterval(mid)) / self.f(triplet).slope
+                # вычисляем обратный интервал к первой производной
+                division = 1 / self.f(triplet).slope
 
-                # чтобы не допустить попадание None в массив результатов, добавляем данное условие
-                if intersection(result, newt_iter) is None:
+                # Если содержится нуль, то интервал распадается на два луча
+                if isinstance(division, list):
+
+                    # обрабатываем каждый луч отдельно (запускаем для них отдельные итерационные процессы)
+                    for i in division:
+                        newt_iter = mid - self.f(mid) * i
+                        print("NEWT OPER: ", newt_iter)
+
+                        # newt_iter = Interval.valueToInterval(mid) - self.f(Interval.valueToInterval(mid)) * i
+
+                        t = NewtonIntervalIterationProcess(self.f,
+                                                           self.func_ext,
+                                                           self.der,
+                                                           intersection(result, newt_iter),
+                                                           self.domain,
+                                                           eps=self.eps)
+                        t.start_with_slope()
+                    break
+                else:
+
+                    # в обычной ситуации действуем по привычному алгоритму
+                    triplet = Triplet(interval=result, value_at_c=Interval.valueToInterval(mid), slope=1)
+                    newt_iter = mid - self.f(mid) / self.f(triplet).slope
+                    print("TRIPLE: ", triplet)
+                    print("NEWT OPER: ", newt_iter)
+                    # newt_iter = Interval.valueToInterval(mid) - self.f(Interval.valueToInterval(mid)) / self.f(triplet).slope
+
+                    # чтобы не допустить попадание None в массив результатов, добавляем данное условие
+                    if intersection(result, newt_iter) is None:
+                        if result.width() <= self.eps:
+                            self.domain.common_base.append(result)
+                        break
+
+                    result = intersection(result, newt_iter)
+                    print("RESULT: ", result)
                     if result.width() <= self.eps:
                         self.domain.common_base.append(result)
-                    break
-
-                result = intersection(result, newt_iter)
-                print("RESULT: ", result)
-                if result.width() <= self.eps:
-                    self.domain.common_base.append(result)
-                    break
+                        break
 
 
 # аккумулирующий класс с хранилищем результатов итерационных процессов
 class NewtonComputations:
 
-    common_base = []
+    # common_base = []
 
     def __init__(self, f, func_ext, first_der, primary_interval: Interval, eps: float):
         self.f = f
@@ -167,6 +280,7 @@ class NewtonComputations:
         self.der = IntervalExtensionFunction(first_der)
         self.primary_interval = primary_interval
         self.eps = eps
+        self.common_base = []
 
     def run_slope(self):
         nip = NewtonIntervalIterationProcess(self.f, self.func_ext, self.der, self.primary_interval, self, self.eps)
